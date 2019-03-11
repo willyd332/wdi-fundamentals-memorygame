d65ae7a463e4f2483d7a797e8128f279384d74a6
WillyD <sopwill3@gmail.com>
2019-03-11 14:29:48 -0600

    created game logic

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -1,6 +1,22 @@
-var cardOne = "queen";
-var cardTwo = "queen"
-var cardThree = "king"
-var cardFour = "king"
+// var cardOne = "queen";
+// var cardTwo = "queen"
+// var cardThree = "king"
+// var cardFour = "king"
+
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+var cardOne = cards[0];
+var cardTwo = cards[2];
+
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
+
+if (cardsInPlay.length === 2) {
+  if (cardsInPlay[0] === cardsInPlay[1]) {
+    alert("It's A Match!")
+  } else {
+    alert("So Close! Try Again.")
+  }
+}
 
 console.log("User Flipped" + cardOne);
